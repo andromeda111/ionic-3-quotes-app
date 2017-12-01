7fe46ee24d67eb585b7a43b3e047143242caeadd
andromeda111 <jmseymour111@gmail.com>
2017-11-30 21:40:19 -0800

    added tabs functionality, quotes data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { LibraryPage } from '../pages/library/library';
 import { QuotesPage } from '../pages/quotes/quotes';
 import { QuotePage } from '../pages/quote/quote';
 import { SettingsPage } from '../pages/settings/settings';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
+    TabsPage,
     FavoritesPage,
     LibraryPage,
     QuotesPage,
@@ -27,6 +29,7 @@ import { SettingsPage } from '../pages/settings/settings';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    TabsPage,
     FavoritesPage,
     LibraryPage,
     QuotesPage,
